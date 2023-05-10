a658fbfe217b7a7d838f5356b2fe63503bbba191
m-e-l-i-s-s-a <57683305+m-e-l-i-s-s-a@users.noreply.github.com>
2023-05-10 09:55:46 -0700

    PlayerControl + TileMap
    Created player control script and tile map.

---
+++ FinalPeriod1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}

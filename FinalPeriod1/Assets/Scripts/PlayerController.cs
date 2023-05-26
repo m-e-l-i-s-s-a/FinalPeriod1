99a01d000f4ea221582c6dd78356b0c5dd616868
m-e-l-i-s-s-a <57683305+m-e-l-i-s-s-a@users.noreply.github.com>
2023-05-26 09:37:29 -0700

    Animator?
    not done... :(

--- FinalPeriod1/Assets/Scripts/PlayerController.cs
+++ FinalPeriod1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
     }
 
